c135d41e002467012c2eae8d1f1eb40fe11c371a
Alejo <alejofilartiga@gmail.com>
2025-04-25 01:14:22 -0300

    Fix Cors

--- src/axios/axios-orders.js
+++ src/axios/axios-orders.js
@@ -6,7 +6,12 @@ import { BASE_URL } from "../Utils/constants";
 
 export const createOrder = async (order, dispatch) => {
     try {
-      const response = await axios.post(`${BASE_URL}`, order);
+      const response = await axios.post(`${BASE_URL}`, order, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true, // Incluir credenciales si es necesario
+      });
       if (response) {
         console.log(response.data);
       }
